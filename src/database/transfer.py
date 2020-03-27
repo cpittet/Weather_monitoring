cdaa161aa2937b22289cddae0a2bdd6fde53e9db
Cyrille Pittet <cyrille.pittet@epfl.ch>
2020-03-27 15:44:18 +0100

    cleaned the repo

--- src/database/transfer.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3
-
-# Last update : 19.03.2020, Cyrille Pittet
-
-# This script :
-#   1) Transfers the data from the data directory at the root of the project
-#      to the influxdb server
-#   2) If they are successfully added to the db, it returns 0, otherwise 1
